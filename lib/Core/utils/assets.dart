8e90cbb5088e7c1c4a43b8d84855342963b7ba60
Mohammed Hafiz <Mohammed.Hafiz.2710@gmail.com>
2025-09-23 15:38:42 +0300

    assets file done

---
+++ lib/Core/utils/assets.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesBanana
+  /// assest/images/banana.svg
+  static const String imagesBanana = "assest/images/banana.svg";
+
+  /// Assets for imagesBasket
+  /// assest/images/basket.svg
+  static const String imagesBasket = "assest/images/basket.svg";
+
+  /// Assets for imagesGrabber
+  /// assest/images/Grabber.svg
+  static const String imagesGrabber = "assest/images/Grabber.svg";
+
+  /// Assets for imagesImageFruits
+  /// assest/images/image_fruits.svg
+  static const String imagesImageFruits = "assest/images/image_fruits.svg";
+
+  /// Assets for imagesLaundry
+  /// assest/images/laundry.svg
+  static const String imagesLaundry = "assest/images/laundry.svg";
+
+  /// Assets for imagesMilkEgg
+  /// assest/images/milk_egg.svg
+  static const String imagesMilkEgg = "assest/images/milk_egg.svg";
+
+  /// Assets for imagesOrange
+  /// assest/images/orange.svg
+  static const String imagesOrange = "assest/images/orange.svg";
+
+  /// Assets for imagesPepper
+  /// assest/images/pepper.svg
+  static const String imagesPepper = "assest/images/pepper.svg";
+
+  /// Assets for imagesSlider 1
+  /// assest/images/Slider 1.svg
+  static const String imagesSlider1 = "assest/images/Slider 1.svg";
+
+  /// Assets for imagesSlider 2
+  /// assest/images/Slider 2.svg
+  static const String imagesSlider2 = "assest/images/Slider 2.svg";
+
+  /// Assets for imagesSlider 3
+  /// assest/images/Slider 3.svg
+  static const String imagesSlider3 = "assest/images/Slider 3.svg";
+
+  /// Assets for imagesVegetables
+  /// assest/images/vegetables.svg
+  static const String imagesVegetables = "assest/images/vegetables.svg";
+}
